e8a816073d7a8fcce931f7426ae86a91761aa6b2
James Coyle <jamesrichardcoyle@gmail.com>
2020-02-09 22:50:29 +0000

    Create icon.vue

---
+++ lib/icon.vue
@@ -0,0 +1,20 @@
+<template>
+	<svg width="24" height="24" viewBox="0 0 24 24">
+		<title v-if="title">{{ title }}</title>
+		<desc v-if="desc">{{ desc }}</desc>
+		<path :d="path" :fill="color" />
+	</svg>
+</template>
+
+<script>
+export default {
+	name: 'icon',
+
+	props: {
+		path: String,
+		color: String,
+		title: String,
+		description: String,
+	},
+}
+</script>
