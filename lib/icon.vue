77ec54a71213aa3f18629bf8d1c7d94844ccb1f2
James Coyle <jamesrichardcoyle@gmail.com>
2020-02-09 22:56:22 +0000

    Fix description binding

--- lib/icon.vue
+++ lib/icon.vue
@@ -1,7 +1,7 @@
 <template>
 	<svg width="24" height="24" viewBox="0 0 24 24">
 		<title v-if="title">{{ title }}</title>
-		<desc v-if="desc">{{ desc }}</desc>
+		<desc v-if="description">{{ description }}</desc>
 		<path :d="path" :fill="color" />
 	</svg>
 </template>
